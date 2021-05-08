15320eda2c64b346496f77563f25d718e25697c5
JayZ <jayit1@hotmail.com>
2021-05-08 19:19:52 +0530

    Home Screen finish + LOGO designed

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -3,67 +3,99 @@
  * https://reactnavigation.org/docs/bottom-tab-navigator
  */
 
- import {
-  Ionicons,
-  Entypo,
-  EvilIcons,
-  MaterialCommunityIcons,
-  FontAwesome5
-} from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
-import * as React from 'react';
+import {
+	Ionicons,
+	Entypo,
+	EvilIcons,
+	MaterialCommunityIcons,
+	FontAwesome5,
+} from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import * as React from "react";
 
-import Colors from '../constants/Colors';
-import useColorScheme from '../hooks/useColorScheme';
-import HomeScreen from '../screens/HomeScreen';
-import TabTwoScreen from '../screens/TabTwoScreen';
-import { BottomTabParamList, TabOneParamList, TabTwoParamList } from '../types';
-
+import Colors from "../constants/Colors";
+import useColorScheme from "../hooks/useColorScheme";
+import HomeScreen from "../screens/HomeScreen";
+import TabTwoScreen from "../screens/TabTwoScreen";
+import { BottomTabParamList, TabOneParamList, TabTwoParamList } from "../types";
+import { View, Image, Text, StyleSheet } from "react-native";
 const BottomTab = createBottomTabNavigator<BottomTabParamList>();
 
 export default function BottomTabNavigator() {
-  const colorScheme = useColorScheme();
+	const colorScheme = useColorScheme();
 
-  return (
-    <BottomTab.Navigator
-      initialRouteName="TabOne"
-      tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}>
-      <BottomTab.Screen
-        name="Home"
-        component={TabOneNavigator}
-        options={{
-          tabBarIcon: ({ color }) => <Entypo name="home" size={25} style={{ marginBottom: -3 }} color={color} />,
-        }}
-      />
-      <BottomTab.Screen
-        name="Search"
-        component={TabTwoNavigator}
-        options={{
-          tabBarIcon: ({ color }) => <EvilIcons name="search" size={25} style={{ marginBottom: -3 }} color={color} />,
-        }}
-      />
-      <BottomTab.Screen
-        name="Your Library"
-        component={TabTwoNavigator}
-        options={{
-          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="bookshelf" size={25} style={{ marginBottom: -3 }} color={color} />,
-        }}
-      />
-      <BottomTab.Screen
-        name="Premium"
-        component={TabTwoNavigator}
-        options={{
-          tabBarIcon: ({ color }) => <FontAwesome5 name="spotify" size={22} style={{ marginBottom: -3 }} color={color} />,
-        }}
-      />
-    </BottomTab.Navigator>
-  );
+	return (
+		<BottomTab.Navigator
+			initialRouteName="TabOne"
+			tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}
+		>
+			<BottomTab.Screen
+				name="Home"
+				component={TabOneNavigator}
+				options={{
+					tabBarIcon: ({ color }) => (
+						<Entypo
+							name="home"
+							size={25}
+							style={{ marginBottom: -3 }}
+							color={color}
+						/>
+					),
+				}}
+			/>
+			<BottomTab.Screen
+				name="Search"
+				component={TabTwoNavigator}
+				options={{
+					tabBarIcon: ({ color }) => (
+						<EvilIcons
+							name="search"
+							size={25}
+							style={{ marginBottom: -3 }}
+							color={color}
+						/>
+					),
+				}}
+			/>
+			<BottomTab.Screen
+				name="Your Library"
+				component={TabTwoNavigator}
+				options={{
+					tabBarIcon: ({ color }) => (
+						<MaterialCommunityIcons
+							name="bookshelf"
+							size={25}
+							style={{ marginBottom: -3 }}
+							color={color}
+						/>
+					),
+				}}
+			/>
+			<BottomTab.Screen
+				name="Premium"
+				component={TabTwoNavigator}
+				options={{
+					tabBarIcon: ({ color }) => (
+						<FontAwesome5
+							name="spotify"
+							size={22}
+							style={{ marginBottom: -3 }}
+							color={color}
+						/>
+					),
+				}}
+			/>
+		</BottomTab.Navigator>
+	);
 }
 // You can explore the built-in icon families and icons on the web at:
 // https://icons.expo.fyi/
-function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']; color: string }) {
-  return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
+function TabBarIcon(props: {
+	name: React.ComponentProps<typeof Ionicons>["name"];
+	color: string;
+}) {
+	return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
 }
 
 // Each tab has its own navigation stack, you can read more about this pattern here:
@@ -71,27 +103,50 @@ function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']
 const TabOneStack = createStackNavigator<TabOneParamList>();
 
 function TabOneNavigator() {
-  return (
-    <TabOneStack.Navigator>
-      <TabOneStack.Screen
-        name="HomeScreen"
-        component={HomeScreen}
-        options={{ headerTitle: 'Tab One Title' }}
-      />
-    </TabOneStack.Navigator>
-  );
+	return (
+		<TabOneStack.Navigator>
+			<TabOneStack.Screen
+				name="HomeScreen"
+				component={HomeScreen}
+				// options={{ headerTitle: "hi" }}
+				options={{
+					headerTitle: (
+						props // App Logo
+					) => (
+						<View
+							style={{
+								justifyContent: "center",
+								alignItems: "center",
+							}}
+						>
+							<Image
+								style={{
+									width: 200,
+									height: 50,
+									// marginLeft: -50,
+		   						}}
+								source={require("../assets/images/spotify1.png")}
+								resizeMode="contain"
+							/>
+						</View>
+					),
+					// headerTitleStyle: { flex: 1, textAlign: "left" },
+				}}
+			/>
+		</TabOneStack.Navigator>
+	);
 }
 
 const TabTwoStack = createStackNavigator<TabTwoParamList>();
 
 function TabTwoNavigator() {
-  return (
-    <TabTwoStack.Navigator>
-      <TabTwoStack.Screen
-        name="TabTwoScreen"
-        component={TabTwoScreen}
-        options={{ headerTitle: 'Tab Two Title' }}
-      />
-    </TabTwoStack.Navigator>
-  );
+	return (
+		<TabTwoStack.Navigator>
+			<TabTwoStack.Screen
+				name="TabTwoScreen"
+				component={TabTwoScreen}
+				options={{ headerTitle: "Tab Two Title" }}
+			/>
+		</TabTwoStack.Navigator>
+	);
 }
