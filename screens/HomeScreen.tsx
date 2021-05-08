28d45a82647e3339a3b25233da86343e205761f4
JayZ <jayit1@hotmail.com>
2021-05-08 17:59:38 +0530

    Home Screen perfected, filled with Album Category and Albums

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,52 +1,37 @@
 import * as React from "react";
 import { StyleSheet, FlatList } from "react-native";
 import AlbumCategory from "../components/AlbumCategory";
-import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
-// const album = {
-// 	id: "1",
-// 	imageUri:
-// 		"https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg",
-// 	artistsHeadline: "Taylor Swift, Kygo Objective C, Avicii",
-// };
+import albumCategories from "../data/albumCategories";
 
-const albumCategory = {
-	id: "1",
-	title: "Happy Vibes",
-	albums: [
-		{
-			id: "1",
-			imageUri:
-				"https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg",
-			artistsHeadline: "Taylor Swift, Kygo Objective C, Avicii",
-		},
-		{
-			id: "2",
-			imageUri:
-				"https://cdn6.f-cdn.com/contestentries/1485199/27006121/5ca3e39ced7f1_thumb900.jpg",
-			artistsHeadline: "Post Malone, Drake, Eminem",
-		},
-		{
-			id: "3",
-			imageUri:
-				"https://images-na.ssl-images-amazon.com/images/I/61F66QURFyL.jpg",
-			artistsHeadline: "Journey, Escape, Avicii",
-		},
-		{
-			id: "4",
-			imageUri:
-				"https://i.pinimg.com/originals/a2/0d/37/a20d37791f8ad5cd54734cd3af559cc9.jpg",
-			artistsHeadline: "Bob Marley, Cardi B, Stas Mihailov",
-		},
-	],
-};
 export default function HomeScreen() {
 	return (
-		<View>
-			<AlbumCategory
-				title={albumCategory.title}
-				albums={albumCategory.albums}
+		<View style={styles.container}>
+		
+			<FlatList
+				data={albumCategories}
+				renderItem={({ item }) => (
+					<AlbumCategory title={item.title} albums={item.albums} />
+				)}
+				keyExtractor={(item) => item.id}
+				// showsHorizontalScrollIndicator={false}
 			/>
 		</View>
 	);
 }
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: "bold",
+	},
+	separator: {
+		marginVertical: 30,
+		height: 1,
+		width: "80%",
+	},
+});
