58c7251bbeb1121364bbe74e2000f5cc1faaee21
JayZ <jayit1@hotmail.com>
2021-05-08 20:05:23 +0530

    AlbumScreen Song List Item -- Complete

---
+++ components/SongListItem/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Text, Image, View, StyleSheet } from "react-native";
+
+import { Song } from "../../types";
+
+export type SongListItemProps = {
+	song: Song;
+};
+
+const SongListItem = (props: SongListItemProps) => {
+	const { song } = props;
+	return (
+		<View style={styles.container}>
+			<Image source={{ uri: song.imageUri }} style={styles.image} />
+			<View style={styles.rightContainer}>
+				<Text style={styles.title}>{song.title}</Text>
+				<Text style={styles.artist}>{song.artist}</Text>
+			</View>
+		</View>
+	);
+};
+const styles = StyleSheet.create({
+	container: {
+		flexDirection: "row",
+		margin: 15,
+	},
+	image: {
+		width: 75,
+		height: 75,
+	},
+	rightContainer: {
+		justifyContent: "space-around",
+		marginLeft: 15,
+	},
+	title: {
+		color: "white",
+		fontSize: 24,
+	},
+	artist: {
+		color: "lightgray",
+		fontSize: 20,
+	},
+});
+export default SongListItem;
